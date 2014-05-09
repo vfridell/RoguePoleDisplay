ccaa3fdae0dc879faa475ee211e70d98234cf78d
vfridell <gusknows@gmail.com>
2014-05-09 17:26:14 -0400

    Updated ConsoleInput to use new InputListener

--- Input/ConsoleInput.cs
+++ Input/ConsoleInput.cs
@@ -4,91 +4,73 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using RoguePoleDisplay.InputListeners;
 
 namespace RoguePoleDisplay.Input
 {
     public class ConsoleInput : IGetInput
     {
+        Interaction _interaction;
+        bool _interactionHappened = false;
+        AutoResetEvent getInput = new AutoResetEvent(false);
+
         public int GetInteger(int millisecondTimeout)
         {
-            string val = Reader.ReadLine(millisecondTimeout);
-            int intResult;
-            if (int.TryParse(val, out intResult))
-                return intResult;
+            using (var consoleListener = new ConsoleListener())
+            {
+                _interaction = new Interaction();
+                consoleListener.InteractionHandled += consoleListener_InteractionHandled;
+                consoleListener.Start();
+                getInput.WaitOne(millisecondTimeout);
+                return _interaction.resultValue;
+            }
+        }
 
-            throw new Exception("Could not parse integer from console input");
+        void consoleListener_InteractionHandled(object sender, InteractionEventArgs e)
+        {
+            lock (_interaction)
+
+            {
+                _interaction = e.interaction;
+                _interactionHappened = true;
+                getInput.Set();
+            }
         }
 
         public bool TryGetInteger(out int value, int millisecondTimeout)
         {
-            string val = Reader.ReadLine(millisecondTimeout);
-            return int.TryParse(val, out value);
+            using (var consoleListener = new ConsoleListener())
+            {
+                _interactionHappened = false;
+                _interaction = new Interaction();
+                consoleListener.InteractionHandled += consoleListener_InteractionHandled;
+                consoleListener.Start();
+                getInput.WaitOne(millisecondTimeout);
+                value = _interaction.resultValue;
+                return _interactionHappened;
+            }
         }
 
         public void Init()
         {
-            Reader.Start();
         }
 
         public MenuItem ChooseFromMenu(Menu menu, int millisecondTimeout)
         {
-            int choice;
-            Console.Write("Enter choice: ");
-            string val = Reader.ReadLine(millisecondTimeout);
-            if (!int.TryParse(val, out choice) || !menu.ValidChoice(choice))
-                return menu.GetMenuItem(choice);
-            else
-                return null;
+            using (var consoleListener = new ConsoleListener())
+            {
+                _interactionHappened = false;
+                _interaction = new Interaction();
+                consoleListener.InteractionHandled += consoleListener_InteractionHandled;
+                consoleListener.Start();
+                Console.Write("Enter choice: ");
+                getInput.WaitOne(millisecondTimeout);
+                if (menu.ValidChoice(_interaction.resultValue))
+                    return menu.GetMenuItem(_interaction.resultValue);
+                else
+                    return null;
+            }
         }
     }
 
-    /// <summary>
-    /// Uses a thread to allow for a timeout on Console.Readline
-    /// </summary>
-    class Reader
-    {
-        private static Thread _inputThread;
-        private static AutoResetEvent _getInput, _gotInput;
-        private static string _input;
-
-        static Reader()
-        {
-            _inputThread = new Thread(reader);
-            _inputThread.IsBackground = true;
-            _getInput = new AutoResetEvent(false);
-            _gotInput = new AutoResetEvent(false);
-        }
-
-        public static void Start()
-        {
-            _inputThread.Start();
-        }
-
-        private static void reader()
-        {
-            try
-            {
-                while (true)
-                {
-                    _getInput.WaitOne();
-                    _input = Console.ReadLine();
-                    _gotInput.Set();
-                }
-            }
-            catch (Exception ex)
-            {
-                // TODO how to handle?
-            }
-        }
-
-        public static string ReadLine(int timeOutMillisecs)
-        {
-            _getInput.Set();
-            bool success = _gotInput.WaitOne(timeOutMillisecs);
-            if (success)
-                return _input;
-            else
-                return "";
-        }
-    }
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Leap;
-using RoguePoleDisplay.LeapListeners;
 
 namespace RoguePoleDisplay
 {
