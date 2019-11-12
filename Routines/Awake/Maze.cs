ebf2fe0be8d4255f7237bbbaf330918be8bbd1de
vfridell <gusknows@gmail.com>
2019-11-12 14:48:18 -0500

    working on maze

--- Routines/Awake/Maze.cs
+++ Routines/Awake/Maze.cs
@@ -18,17 +18,11 @@ namespace RoguePoleDisplay.Routines
         {
             using (var memory = new Memory())
             {
-                Topic[] topics = {  new Topic() { name = "Ebola", positive=false},
-                                new Topic() { name = "football game", positive=true},
-                                new Topic() { name = "weather", positive=true},
-                             };
-
-                Random rand = new Random();
-                int index = rand.Next(0, 3);
-
                 var face = new Face(RendererFactory.GetPreferredRenderer(), InputFactory.GetPreferredInput());
-                Interaction i = face.RememberSingleValue(memory, "How bout that", topics[index].name);
-                face.Talk(memory, "Uh huh.");
+                Interaction i = new Interaction();
+
+
+
                 return MakeRoutineResult(memory, i);
             }
         }
